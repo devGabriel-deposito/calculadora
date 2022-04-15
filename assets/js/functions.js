349e95811cf22b0dcee8f015b2b39d17a89f6e6c
Gabriel G. Freitas <67321807+gabrielgoncalfreitas@users.noreply.github.com>
2022-04-15 19:16:18 -0300

    Add files via upload

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,128 +1,204 @@
-var virgula = false
-var operar = false
-
 var firstNumber = 0
 var secondNumber = 0
+var virg = false
 
-var numeroResult = '0'
+var lastOperation = 'so'
 
-function format(numberoBkp) {
+
+$(document).ready(function () {
+    $('#result').val(0)
+})
+
+
+function formatAndSet(sNum) {
+    var sBkp = ''
+    var sRtn = ''
+    var bFin = false
+    var bTem = false
+    var nHou = 1
+
+    if (typeof(sNum) == 'number') sNum = sNum.toString()
     
+    for (var num = (sNum.length - 1); num >= 0; num--) if (sNum[num] == ',') bTem
+
+    if (bTem == true) {
+        for (var num = (sNum.length - 1); num >= 0; num--) {
+            if (sNum[num] == ',') {
+                bFin = true
+                sBkp += sNum[num]
+                num--
+            }
+
+            if (bFin == true) {
+                sBkp += sNum[num]
+                if (nHou % 3 == 0 && num > 0) sBkp += '.'
+                nHou++
+            }
+            else if (bFin == false) sBkp += sNum[num]
+        }
+    }
+
+    else if (bTem == false) {
+        for (var num = (sNum.length - 1); num >= 0; num--) {
+            sBkp += sNum[num]
+            if (nHou % 3 == 0 && num > 0) sBkp += '.'
+            nHou++
+        }
+    }
+
+    for (var num = (sBkp.length - 1); num >= 0; num--) sRtn += sBkp[num]
+
+    $('#result').val(sRtn)
 }
 
 function cls() {
-    numeroResult = ''
-    $('#result').val(numeroResult.toLocate)
+    firstNumber = 0
+    secondNumber = 0
+    virg = false
+    formatAndSet(secondNumber)
 }
 
+
 function backspace() {
-    numeroResult = numeroResult.substring(0, numeroResult.length - 1)
-    $('#result').val(numeroResult)
+    secondNumber = secondNumber.toString()
+    secondNumber = secondNumber.substring(0, secondNumber.length - 1)
+    if (secondNumber != '') secondNumber = parseFloat(secondNumber)
+    else secondNumber = 0
+    formatAndSet(secondNumber)
+}
+
+function enter() {
+    if (lastOperation == '') {
+        firstNumber = secondNumber
+        secondNumber = '0'
+    }
+
+    else if (lastOperation != '') {
+        if (lastOperation == 'so') { firstNumber = parseFloat(firstNumber) + parseFloat(secondNumber) }
+        else if (lastOperation == 'su') { firstNumber = parseFloat(firstNumber) - parseFloat(secondNumber) }
+        else if (lastOperation == 'mu') { firstNumber = parseFloat(firstNumber) * parseFloat(secondNumber) }
+        else if (lastOperation == 'di') { firstNumber = parseFloat(firstNumber) / parseFloat(secondNumber) }
+
+        secondNumber = firstNumber
+    }
+
+    formatAndSet(secondNumber)
+}
+
+function virgula() {
+    if (virg == false) {
+        secondNumber = secondNumber.toString()
+        secondNumber += '.'
+        secondNumber = parseFloat(secondNumber)
+        virg = true
+    }
+
+    formatAndSet(secondNumber)
 }
 
 function btn0() {
-    if (numeroResult != 0) {
-        numeroResult += '0'
-        $('#result').val(numeroResult)
+    if (secondNumber != 0) {
+        secondNumber += '0'
+        formatAndSet(secondNumber)
     }
 }
 
 function btn1() {
-    if (numeroResult == 0) {
-        numeroResult = '1'
-        $('#result').val(numeroResult)
+    if (secondNumber == 0) {
+        secondNumber = '1'
+        formatAndSet(secondNumber)
     }
     else {
-        numeroResult += '1'
-        $('#result').val(numeroResult)
+        secondNumber += '1'
+        formatAndSet(secondNumber)
     }
 }
 
+
 function btn2() {
-    if (numeroResult == 0) {
-        numeroResult = '2'
-        $('#result').val(numeroResult)
+    if (secondNumber == 0) {
+        secondNumber = '2'
+        formatAndSet(secondNumber)
     }
     else {
-        numeroResult += '2'
-        format()
-        $('#result').val(numeroResult)
+        secondNumber += '2'
+        formatAndSet(secondNumber)
     }
 }
 
 function btn3() {
-    if (numeroResult == 0) {
-        numeroResult = '3'
-        $('#result').val(numeroResult)
+    if (secondNumber == 0) {
+        secondNumber = '3'
+        formatAndSet(secondNumber)
     }
     else {
-        numeroResult += '3'
-        $('#result').val(numeroResult)
+        secondNumber += '3'
+        formatAndSet(secondNumber)
     }
 }
 
 function btn4() {
-    if (numeroResult == 0) {
-        numeroResult = '4'
-        $('#result').val(numeroResult)
+    if (secondNumber == 0) {
+        secondNumber = '4'
+        formatAndSet(secondNumber)
     }
     else {
-        numeroResult += '4'
-        $('#result').val(numeroResult)
+        secondNumber += '4'
+        formatAndSet(secondNumber)
     }
 }
 
 function btn5() {
-    if (numeroResult == 0) {
-        numeroResult = '5'
-        $('#result').val(numeroResult)
+    if (secondNumber == 0) {
+        secondNumber = '5'
+        formatAndSet(secondNumber)
     }
     else {
-        numeroResult += '5'
-        $('#result').val(numeroResult)
+        secondNumber += '5'
+        formatAndSet(secondNumber)
     }
 }
 
 function btn6() {
-    if (numeroResult == 0) {
-        numeroResult = '6'
-        $('#result').val(numeroResult)
+    if (secondNumber == 0) {
+        secondNumber = '6'
+        formatAndSet(secondNumber)
     }
     else {
-        numeroResult += '6'
-        $('#result').val(numeroResult)
+        secondNumber += '6'
+        formatAndSet(secondNumber)
     }
 }
 
 function btn7() {
-    if (numeroResult == 0) {
-        numeroResult = '7'
-        $('#result').val(numeroResult)
+    if (secondNumber == 0) {
+        secondNumber = '7'
+        formatAndSet(secondNumber)
     }
     else {
-        numeroResult += '7'
-        $('#result').val(numeroResult)
+        secondNumber += '7'
+        formatAndSet(secondNumber)
     }
 }
 
 function btn8() {
-    if (numeroResult == 0) {
-        numeroResult = '8'
-        $('#result').val(numeroResult)
+    if (secondNumber == 0) {
+        secondNumber = '8'
+        formatAndSet(secondNumber)
     }
     else {
-        numeroResult += '8'
-        $('#result').val(numeroResult)
+        secondNumber += '8'
+        formatAndSet(secondNumber)
     }
 }
 
 function btn9() {
-    if (numeroResult == 0) {
-        numeroResult = '9'
-        $('#result').val(numeroResult)
+    if (secondNumber == 0) {
+        secondNumber = '9'
+        formatAndSet(secondNumber)
     }
     else {
-        numeroResult += '9'
-        $('#result').val(numeroResult)
+        secondNumber += '9'
+        formatAndSet(secondNumber)
     }
 }
